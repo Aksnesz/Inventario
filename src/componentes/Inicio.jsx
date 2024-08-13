9031d9c89705b5b0db95ca5c3a27dcfbb5955b72
Diaflosno <al22304048@utslrc.edu.mx>
2024-08-13 11:14:43 -0700

    Agregando navbar

---
+++ src/componentes/Inicio.jsx
@@ -0,0 +1,15 @@
+import './contenido.css'; // Asegúrate de crear un archivo CSS para estilos
+
+
+function inicio() {
+
+    return (
+      <>
+      <div className="contenido">
+<p>ola</p>
+      </div>
+     </>
+    )
+  }
+  
+  export default inicio
